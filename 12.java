40b140013111307b372e996b94b864969d5774c2
karthik8116k <sagarkarthik741636@gmail.com>
2025-01-08 16:11:33 +0530

    Add files via upload

---
+++ 12.java
@@ -0,0 +1,14 @@
+class A
+{
+int a=200;
+public static void main(String[]args){
+A ob = new A();
+int x=10;
+int y=20;
+String k=300;
+String b=400;
+System.out.println(ob.a);
+System.out.println(x+y);
+System.out.println(k+"   "+b);
+}
+}
